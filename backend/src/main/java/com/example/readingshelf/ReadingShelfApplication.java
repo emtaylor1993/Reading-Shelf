d26657f97517088e0c4075c31e73819cf259a290
etaylor5 <etaylorsubs1993@gmail.com>
2026-09-11 16:52:46 -0400

    -- initial commit

---
+++ backend/src/main/java/com/example/readingshelf/ReadingShelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.readingshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingShelfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/readingshelf/ReadingShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readingshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
